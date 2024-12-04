93688ba2cc98a096f6bdf47eb56b9421fa46351c
alfacad786 <alfa_cad786@yahoo.com>
2024-12-04 14:17:33 +0530

    remove before commit

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,17 +1,20 @@
 module.exports = {
-    apps: [
-      {
-        name: 'index.js',            // Application ka naam
-        script: 'index.js',         // Run hone wali file ka naam
-        watch: true,                // Watch mode enable karein
-        ignore_watch: ['node_modules', 'logs'], // Ignore folders
-        env: {
-          NODE_ENV: 'development',
-        },
-        env_production: {
-          NODE_ENV: 'production',
-        },
+  apps: [
+    {
+      name: 'backend',
+      script: 'index.js',
+      watch: true, // Enable watch mode
+      ignore_watch: ['node_modules', 'logs'], // Ignore unnecessary folders
+      watch_options: {
+        followSymlinks: false,
+        usePolling: true, // Enable polling to detect file changes more reliably
       },
-    ],
-  };
-  
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
