21d1520e0f853a6083cba467863a8f16b9f2b1e2
alfacad786 <alfa_cad786@yahoo.com>
2024-12-04 14:45:47 +0530

    add app.use((req, res, next) => {   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');   next(); });

--- index.js
+++ index.js
@@ -110,6 +110,10 @@ app.use(cors({
     }
   }
 }));
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  next();
+});
 
 // ==================================================
 
